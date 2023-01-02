31c68e3debbd62ee2bb2bfc49f861a0999cd9dde
Olivier Laurent <olivier.laurent@ens-lyon.fr>
2023-01-02 09:39:22 +0100

    cleaning

--- macrollcut.v
+++ macrollcut.v
@@ -3,6 +3,6 @@ From NanoYalla Require Export nanollcut.
 Lemma cut_r_ext l1 A l2 : ll (l1 ++ A :: nil) -> ll (dual A :: l2) -> ll (l1 ++ l2).
 Proof.
 intros pi1%(ex_transp_middle2 nil) pi2.
-replace (l1 ++ nil) with l1 in pi1; [ | rewrite List.app_nil_r; reflexivity ].
+rewrite List.app_nil_r in pi1.
 apply (cut_r A); assumption.
 Defined.
